d1be8211c54855933fcd31ddb6d17ada046e0d55
Patricia <patsmrkt@gmail.com>
2017-07-05 09:38:22 -0400

    Search and info page skeleton
    - search page: has title and search bar where user can search a book
    - info page: displays title of book and the author

--- mainCtrl.js
+++ mainCtrl.js
@@ -1,3 +1,29 @@
-app.controller("mainCtrl", function($scope) {
+app.config(function($routeProvider){
+  $routeProvider
+  .when("/", {
+    templateUrl: "home.html"
+  })
+  .when("/bookInfo", {
+    templateUrl: "bookinfo.html"
+  });
+});
+
+app.controller("mainCtrl", function($scope, $location) {
+  $scope.books ={"harry potter" : "jk rowling", "the book thief" : "markus zusak"};
+  $scope.title = "booklr";
+
+  function search(key){
+    if ($scope.books[key] === undefined)
+      {$scope.msg = "Sorry, book not found";}
+    else {
+      $scope.msg = "author: " + $scope.books[key];
+    }
+  };
+
+  $scope.submit = function(findBook) {
+    $scope.book = findBook;
+    search(findBook);
+    $location.path("/bookInfo");
+  };
 
 });
