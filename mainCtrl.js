1d1638470c3fd4c3b815cd03403430f617a37276
Patricia <patsmrkt@gmail.com>
2017-07-07 15:35:40 -0400

    Include search suggestions
    - search suggestions appear when user types into search box
    - suggestions in both home page and navi search bar

--- mainCtrl.js
+++ mainCtrl.js
@@ -8,28 +8,36 @@ app.config(function($routeProvider){
   });
 });
 
-app.controller("mainCtrl", function($scope, $location, $http) {
+app.controller("mainCtrl", function($scope, $location, $http, $window) {
   $scope.books ={"harry potter" : "jk rowling", "the book thief" : "markus zusak"};
   $scope.title = "booklr";
   $scope.showMini = false;
+  $scope.findBook = "";
 
+  // Get data from MySQL database
   $http.get('http://localhost:9000/book_demo.php').then(function (response) {
     $scope.bookDemo = response.data.records;
   });
 
+  // Performs search from user input
   function search(key){
-    if ($scope.books[key] === undefined){
-      $scope.msg = "Sorry, book not found";
-      $scope.booktitle = "";
-      $scope.author = "";
+    for (var i = 0; i < $scope.bookDemo.length; i++){
+      if ($scope.bookDemo[i].Title.toLowerCase() === key.toLowerCase()){
+        $scope.msg = "Showing results for " + key;
+        $scope.booktitle = "title: " + $scope.bookDemo[i].Title;
+        $scope.author = "author: " + $scope.bookDemo[i].Author;
+        $scope.genre = "genre: " + $scope.bookDemo[i].Description;
+        return;
+      }
     }
-    else {
-      $scope.msg = "Showing results for " + key;
-      $scope.booktitle = "title: " + key;
-      $scope.author = "author: " + $scope.books[key];
-    }
+
+    $scope.msg = "Sorry, book not found";
+    $scope.booktitle = "";
+    $scope.author = "";
+    $scope.genre = "";
   };
 
+  // When user enters something into serach bar
   $scope.submit = function(findBook) {
     $scope.book = findBook;
     search(findBook);
@@ -37,8 +45,16 @@ app.controller("mainCtrl", function($scope, $location, $http) {
     $scope.findBook = "";
   };
 
+  // Toggles search bar in navigation bar
   $scope.showSearch = function() {
     $scope.showMini = !$scope.showMini;
+    var elem = $window.document.getElementById('navisearch');
+    elem.focus();
+  }
+
+  $scope.showSuggestions = function(search) {
+    if (search !== "") {return true;}
+    else {return false;}
   }
 
 });
